41a4575a7764fc8ab33ee440a59546e6e80f7ffc
Anerisse <aleksandraneri165@gmail.com>
2026-03-10 16:52:00 +0700

    create Movie Dao

---
+++ app/src/main/java/com/example/wmc_wewatch/data/MovieDao.kt
@@ -0,0 +1,30 @@
+package com.example.wmc_wewatch.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+    //Получить все фильмы
+    @Query("SELECT * FROM movies")
+    fun getAllMovies(): Flow<List<Movie>>
+
+    //Вставить фильм
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: Movie)
+
+    //Обновить фильм
+    @Update
+    suspend fun updateMovie(movie: Movie)
+
+    @Delete
+    suspend fun deleteMovie(movie: Movie)
+
+    @Query("DELETE FROM movies WHERE isSelected = 1")
+    suspend fun deleteSelectedMovies()
+}
